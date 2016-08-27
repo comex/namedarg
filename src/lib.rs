6376dcd7a16cb781a66d9a6d19d3536a4fd5daf6
comex <comexk@gmail.com>
2016-08-27 19:01:10 -0400

    @ won't work

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ fn passthrough_items(cx: &mut ExtCtxt, args: &[TokenTree])
     if parser.token != token::Eof {
         let token_str = parser.this_token_to_string();
         let msg = format!("extra tokens starting with `{}`", token_str);
-        parser.diagnostic().struct_span_err(parser.span, &msg).emit();
+        parser.diagnostic().span_err(parser.span, &msg);
     }
     MacEager::items(items)
 }
@@ -49,7 +49,7 @@ fn mutate_name<'a, I>(fn_name_ident: &ast::Ident, arg_names: I) -> ast::Ident
 
 }
 
-fn transform_this_fn(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
+fn transform_this_fn(args: &[TokenTree], cx: &mut ExtCtxt) -> (Option<Vec<TokenTree>>, usize) {
     let first = &args[0];
     let ident = if let &TokenTree::Token(_, token::Ident(ref ident)) = first {
         ident
@@ -93,10 +93,10 @@ fn transform_this_fn(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
     #[derive(Debug)]
     struct Arg<'a> {
         comma: Option<&'a TokenTree>,
+        attrs: &'a [TokenTree],
         name: Option<&'a ast::Ident>,
         pattern: Option<&'a [TokenTree]>,
         ty: &'a [TokenTree],
-        default: Option<&'a [TokenTree]>,
     }
     let mut parsed_out: Vec<Arg> = Vec::new();
     let mut remaining = tts;
@@ -106,24 +106,32 @@ fn transform_this_fn(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
         let mut next: &[TokenTree] = &[];
         let mut next_comma: Option<&TokenTree> = None;
         let mut colon_idx: Option<usize> = None;
-        let mut eq_idx: Option<usize> = None;
-        for (i, tt) in remaining.iter().enumerate() {
+        let mut attr_index: usize = 0;
+        while let Some(&TokenTree::Token(_, token::Pound)) = this_arg.get(0) {
+            if let Some(&TokenTree::Delimited(_, ref attr_delimed)) = this_arg.get(1) {
+                if attr_delimed.delim != token::DelimToken::Bracket { break }
+                // ok, this looks like an attr
+                // todo: look for #[default]
+                attr_index += 2;
+                this_arg = &this_arg[2..];
+            } else { break }
+        }
+        let attrs: &[TokenTree] = &remaining[..attr_index];
+        for (i, tt) in this_arg.iter().enumerate() {
             match tt {
                 &TokenTree::Token(_, token::Comma) => {
-                    this_arg = &remaining[..i];
-                    next_comma = Some(&remaining[i]);
-                    next = &remaining[i+1..];
+                    next_comma = Some(&this_arg[i]);
+                    next = &this_arg[i+1..];
+                    this_arg = &this_arg[..i];
                     break;
                 },
                 &TokenTree::Token(_, token::Colon) => {
                     if colon_idx.is_none() { colon_idx = Some(i); }
                 },
-                &TokenTree::Token(_, token::Eq) => {
-                    if colon_idx.is_some() && eq_idx.is_none() { eq_idx = Some(i); }
-                },
                 _ => (),
             }
         }
+
         let name: Option<&ast::Ident> = match this_arg.get(1) {
             Some(&TokenTree::Token(_, token::Ident(ref ident2))) =>
                 match this_arg.get(0) {
@@ -131,7 +139,13 @@ fn transform_this_fn(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
                         this_arg = &this_arg[1..];
                         Some(ident)
                     },
-                    Some(&TokenTree::Token(_, token::Underscore)) => {
+                    Some(&TokenTree::Token(underscore_span, token::Underscore)) => {
+                        if colon_idx != Some(2) {
+                            if let Some(&TokenTree::Token(_, token::At)) = this_arg.get(2) {} else {
+                                let msg = "underscore not followed by an identifier binding";
+                                cx.span_err(underscore_span, msg);
+                            }
+                        }
                         this_arg = &this_arg[1..];
                         Some(ident2)
                     },
@@ -139,26 +153,22 @@ fn transform_this_fn(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
                 },
             _ => None
         };
-        let (pattern, ty, default): (Option<&[TokenTree]>, &[TokenTree], Option<&[TokenTree]>) =
-        match (colon_idx, eq_idx) {
-            (Some(colon_idx), Some(eq_idx)) => {
-                (Some(&this_arg[..colon_idx]),
-                 &this_arg[colon_idx..eq_idx],
-                 Some(&this_arg[eq_idx+1..]))
-            },
-            (Some(colon_idx), None) => {
-                let (a, b) = this_arg.split_at(colon_idx);
-                (Some(a), b, None)
-            },
-            _ => (None, this_arg, None)
-        };
-        parsed_out.push(Arg { comma: comma, name: name, pattern: pattern, ty: ty, default: default });
+        let (pattern, ty): (Option<&[TokenTree]>, &[TokenTree]) =
+            match colon_idx {
+                Some(colon_idx) => {
+                    let (a, b) = this_arg.split_at(colon_idx);
+                    (Some(a), b)
+                },
+                _ => (None, this_arg)
+            };
+        parsed_out.push(Arg { attrs: attrs, comma: comma, name: name, pattern: pattern, ty: ty });
         remaining = next;
         comma = next_comma;
     }
     let mutated = mutate_name(fn_name_ident, parsed_out.iter().map(|arg| arg.name));
     let mut replacement_tts: Vec<TokenTree> = Vec::new();
     for (i, arg) in parsed_out.iter().enumerate() {
+        replacement_tts.extend_from_slice(arg.attrs);
         if i != 0 { replacement_tts.push(arg.comma.unwrap().clone()); }
         if let Some(pat) = arg.pattern { replacement_tts.extend_from_slice(pat); }
         replacement_tts.extend_from_slice(arg.ty);
@@ -243,19 +253,19 @@ fn transform_this_invocation(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usi
     (Some(replacement), 2)
 }
 
-fn transform_this(args: &[TokenTree]) -> (Option<Vec<TokenTree>>, usize) {
-    let (x, count) = transform_this_fn(args);
+fn transform_this(args: &[TokenTree], cx: &mut ExtCtxt) -> (Option<Vec<TokenTree>>, usize) {
+    let (x, count) = transform_this_fn(args, cx);
     if count != 0 { return (x, count); }
     transform_this_invocation(args)
 }
 
-fn transform_tts_direct(args: &[TokenTree]) -> Cow<[TokenTree]> {
+fn transform_tts_direct<'a>(args: &'a [TokenTree], cx: &mut ExtCtxt) -> Cow<'a, [TokenTree]> {
     let mut remaining_args = args;
     let mut vec: Vec<TokenTree>;
     loop {
         let len = remaining_args.len();
         if len == 0 { return Cow::Borrowed(args); }
-        let (x, count) = transform_this(remaining_args);
+        let (x, count) = transform_this(remaining_args, cx);
         remaining_args = &remaining_args[count..];
         if let Some(to_add) = x {
             vec = args[..(args.len() - len)].to_vec();
@@ -265,7 +275,7 @@ fn transform_tts_direct(args: &[TokenTree]) -> Cow<[TokenTree]> {
     }
     loop {
         if remaining_args.len() == 0 { return Cow::Owned(vec); }
-        let (x, count) = transform_this(remaining_args);
+        let (x, count) = transform_this(remaining_args, cx);
         if let Some(to_add) = x {
             vec.extend_from_slice(&to_add[..]);
         } else {
@@ -275,15 +285,15 @@ fn transform_tts_direct(args: &[TokenTree]) -> Cow<[TokenTree]> {
     }
 }
 
-fn transform_tts(args: &[TokenTree]) -> Cow<[TokenTree]> {
-    let mut tts: Cow<[TokenTree]> = transform_tts_direct(args);
+fn transform_tts<'a>(args: &'a [TokenTree], cx: &mut ExtCtxt) -> Cow<'a, [TokenTree]> {
+    let mut tts: Cow<[TokenTree]> = transform_tts_direct(args, cx);
     //println!("==> {:?}", tts);
     let mut replacement: Option<(usize, Vec<TokenTree>)> = None;
     let mut mut_start_i: Option<usize> = None;
     if let Cow::Borrowed(btts) = tts {
         for (i, tt) in btts.iter().enumerate() {
             if let &TokenTree::Delimited(_, ref delimed) = tt {
-                let new_inner = transform_tts(&delimed.tts[..]);
+                let new_inner = transform_tts(&delimed.tts[..], cx);
                 if let Cow::Owned(xreplacement) = new_inner {
                     replacement = Some((i, xreplacement));
                     mut_start_i = Some(i + 1);
@@ -306,7 +316,7 @@ fn transform_tts(args: &[TokenTree]) -> Cow<[TokenTree]> {
         for tt in tts.to_mut()[i..].iter_mut() {
             if let &mut TokenTree::Delimited(_, ref mut delimed) = tt {
                 let replacement = {
-                    let new_inner = transform_tts(&delimed.tts[..]);
+                    let new_inner = transform_tts(&delimed.tts[..], cx);
                     if let Cow::Owned(replacement) = new_inner { Some(replacement) } else { None }
                 };
                 if let Some(replacement) = replacement {
@@ -323,7 +333,7 @@ fn transform_tts(args: &[TokenTree]) -> Cow<[TokenTree]> {
 
 fn expand_namedarg(cx: &mut ExtCtxt, _sp: Span, args: &[TokenTree])
         -> Box<MacResult + 'static> {
-    let res = transform_tts(args);
+    let res = transform_tts(args, cx);
     passthrough_items(cx, &res[..])
 }
 
--- tests/test.rs
+++ tests/test.rs
@@ -4,10 +4,12 @@ namedarg! {
     fn named_func(arg foo: i32, _ arg2: i32) -> i32 { foo + arg2 }
     fn right_named_func(arg: i32, _ arg2: i32) -> i32 { arg + arg2 }
     fn left_named_func(_ arg: i32, arg2: i32) -> i32 { arg + arg2 }
+    fn tuple_func(_ arg @ (a, b): (i32, i32)) -> i32 { a + b }
     #[test]
     fn test() {
         assert_eq!(named_func(arg: 5, arg2: 1), 6);
         assert_eq!(right_named_func(5, arg2: 1), 6);
         assert_eq!(left_named_func(arg: 5, 1), 6);
+        assert_eq!(tuple_func(arg: (1, 1)), 2);
     }
 }
